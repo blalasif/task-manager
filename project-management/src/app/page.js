0c0a5c5530f919542ba84b823d89145b3e991b52
blalasif <bilaljee684@gmail.com>
2025-10-07 14:00:31 +0500

    fixed that error

--- project-management/src/app/page.js
+++ project-management/src/app/page.js
@@ -82,7 +82,6 @@ export default function Home() {
   //   setTasks([...tasks, task]);
   // };
 
-  let abs = 'ff;
   return (
     <div className="p-6 flex flex-col md:flex-row gap-6 min-h-screen bg-gray-100">
       {/* Projects Section */}
